162ddb91f3e510231b034b035a738222cdffef47
Ricardo Faria <ricardo.faria@softplan.com.br>
2015-08-10 16:23:28 -0300

    Adicionados exemplos para trabalho com time

--- src/featuresmenores/OptionalNaPratica.java
+++ src/featuresmenores/OptionalNaPratica.java
@@ -1,11 +1,8 @@
 package featuresmenores;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
-import java.util.OptionalInt;
-import java.util.stream.Collectors;
 
 import exemplopratico.ImportacaoArquivo;
 import modelos.Carro;
