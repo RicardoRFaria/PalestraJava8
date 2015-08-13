f4c0d788869436840d22e679d0ae1533e3ae3c79
Ricardo Faria <ricardo.faria@softplan.com.br>
2015-08-13 10:27:59 -0300

    - Adicionado link para palestra no README.md - Adicionados exemplos para novidades do map

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# PalestraJava8
+# Palestra Java 8 na prática
 Projeto com os exemplos utilizados na palestra de java 8
+
+# Slides
+Os slides atrelados a este projeto podem ser encontrados no link https://docs.google.com/presentation/d/1zSPmtdGCpaSozuE3ZjKWDdxkxwDEvYl1XcFvH54yU34/edit?usp=sharing
---
+++ src/collection/NovidadesMap.java
@@ -0,0 +1,63 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import exemplopratico.ImportacaoArquivo;
+import modelos.Carro;
+
+public class NovidadesMap {
+	
+	private static final String MARCA_INEXISTENTE = "MarcaInexistente";
+	private static ImportacaoArquivo importacao;
+
+	static {
+		importacao = new ImportacaoArquivo();
+	}
+
+	public static void main(String... args) {
+		List<Carro> carros = importacao.getCarros();
+		
+		// Preenchendo mapa
+		Map<String, List<Carro>> carrosPorMarca = new HashMap<>();
+		carros.forEach(carro -> carrosPorMarca.computeIfAbsent(carro.getMarca(), elemento -> new ArrayList<>()).add(carro));
+		System.out.println(carros);
+		
+		// Retorno caso não exista
+		carrosPorMarca.getOrDefault(MARCA_INEXISTENTE, new ArrayList<>());
+		
+		// Remova item se chave e valor forem iguais a
+		carrosPorMarca.remove(MARCA_INEXISTENTE, new ArrayList<>());
+	}
+	
+	public static void mainNoModoAntigo() {
+		List<Carro> carros = importacao.getCarros();
+		
+		// Preenchendo mapa
+		Map<String, List<Carro>> carrosPorMarca = new HashMap<>();
+		for (Carro carro : carros) {
+			if (!carrosPorMarca.containsKey(carro.getMarca())) {
+				carrosPorMarca.put(carro.getMarca(), new ArrayList<Carro>());
+			}
+			carrosPorMarca.get(carro.getMarca()).add(carro);
+		}
+		
+		// Retorno caso não exista
+		if (carrosPorMarca.containsKey(MARCA_INEXISTENTE)) {
+			carrosPorMarca.get(MARCA_INEXISTENTE);
+		} else {
+			new ArrayList<Carro>();
+		}
+		
+		// Remova item se chave e valor forem iguais a
+		if (carrosPorMarca.containsKey(MARCA_INEXISTENTE) && carrosPorMarca.get(MARCA_INEXISTENTE).equals(new ArrayList<Carro>())) {
+			carrosPorMarca.remove(MARCA_INEXISTENTE);
+		}
+	}
+
+}
